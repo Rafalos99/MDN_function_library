938feec6cde9290fc44b11d2546913b7a25a310f
kh4dijalud <khadijaludhi@gmail.com>
2022-12-12 20:36:17 +0000

    added square and cube root

---
+++ script.js
@@ -0,0 +1,40 @@
+const input = document.querySelector('.numberInput');
+const para = document.querySelector('p');
+
+function squared(num) {
+    return num * num;
+}
+
+function cubed(num) {
+    return num * num * num;
+}
+
+function factorial(num) {
+    if (num < 0) return undefined;
+    if (num === 0) return 1;
+    let x = num - 1;
+    while (x > 1) {
+        num *= x;
+        x--;
+    }
+    return num;
+}
+
+
+
+input.addEventListener("change", () => {
+    const num = parseFloat(input.value);
+    if (isNaN(num)) {
+        para.textContent = "You need to enter a number!";
+    } else {
+        para.textContent = `${num} squared is ${squared(num)}. `;
+        para.textContent += `${num} cubed is ${cubed(num)}. `;
+        para.textContent += `${num} factorial is ${factorial(num)}. `;
+        // Writing my code to complete assessment
+        //! Note: leave a space before the closing ``
+        para.textContent += `Square root of ${num} is ${Math.sqrt(num)}. `;
+        para.textContent += `Cube root of ${num} is ${Math.cbrt(num)}. `;
+
+    }
+});
+
